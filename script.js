28c787c82c070b4b9090199541d530b68a7e56df
valverdb <131722262+valverdb@users.noreply.github.com>
2023-09-03 15:07:28 -0300

    change avatar image commit

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function toggleMode() {
   const image = document.querySelector("#profile img")
 
   if (html.classList.contains("light")) {
-    image.setAttribute("src", "./assets/avatar-light.png")
+    image.setAttribute("src", "./assets/Avatar.png")
   } else {
     image.setAttribute("src", "./assets/Avatar.png")
   }
